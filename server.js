443ff59b978cc8ccde7df13f395e8a234dbcfc92
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-08-23 00:47:21 +0500

    port defined with nodemon

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id', (req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl', (req,res)=>{image.handleApiCall(req,res)})
-app.listen(process.env.PORT ||3001, () => {
+
+const PORT=process.env.PORT
+app.listen( process.env.PORT || 3001, () => {
     console.log(`app is running on port ${PORT}`)
 })
